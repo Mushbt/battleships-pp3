1752ad9d1f107721320e42e1dacfc6ce098d39cd
Mushbt <mustafa_habet@hotmail.com>
2023-03-05 13:22:37 +0000

    Add the game intro which includes how to play the game and ships the player will have

--- run.py
+++ run.py
@@ -15,3 +15,21 @@ board_coordinates = {
     'G': 6,
     'H': 7
 }
+
+def intro():
+    print("Welcome to battleships!\n")
+    print("Battleships is a strategy type guessing game\n")
+    print("It is played on a ruled grid (In this case 8x8)\n")
+    print("You and the computer will have 5 ships each with different sizes\n")
+    print("The location of the ships will be concealed from each other\n")
+    print("You will take turns calling coordinates trying to sink eachothers ships\n")
+    print("The objective of the game is to try and sink all the computers ships\n")
+    print("When this has been achieved bu either you or the computer, the game will be over")
+
+    print("You will have the following ships in your fleet:\n")
+    print("DESTROYER - 2 BOARD POSITIONS")
+    print("SUBMARINE - 3 BOARD POSITIONS")
+    print("CRUISER - 3 BOARD POSITIONS")
+    print("BATTLESHIP -  4 BOARD POSITIONS")
+    print("CARRIER - 5 BOARD POSITIONS")
+    
