b3a319e79a0d8cc315f2c9b93fbe7414ecf8c99d
Mushbt <mustafa_habet@hotmail.com>
2023-03-05 13:15:16 +0000

    Add dictionary which assigns numbers to letters which will be used as the board coordinates

--- run.py
+++ run.py
@@ -4,3 +4,14 @@ P_BOARD = [[" "] * 8 for i in range(8)]
 C_BOARD = [[" "] * 8 for i in range(8)]
 
 SHIP_LENGTHS = [2, 3, 3, 4, 5]
+
+board_coordinates = {
+    'A': 0,
+    'B': 1,
+    'C': 2,
+    'D': 3,
+    'E': 4,
+    'F': 5,
+    'G': 6,
+    'H': 7
+}
