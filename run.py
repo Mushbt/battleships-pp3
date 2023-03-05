ec80d3589654e1a72478f6497366195bdba3b6ff
Mushbt <mustafa_habet@hotmail.com>
2023-03-05 13:11:11 +0000

    Add player and computer board variables

--- run.py
+++ run.py
@@ -1,3 +1,4 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+# Player game board
+P_BOARD = [[" "] * 8 for i in range(8)]
+# Computer game board
+C_BOARD = [[" "] * 8 for i in range(8)]
